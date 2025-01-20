0eb7e1106250f63f54e210dc35f968f34792769c
Martin Cappelletti Agustín <93543220+inkinvissible@users.noreply.github.com>
2025-01-20 20:19:18 -0300

    Update questionsService.js

--- src/services/questionsService.js
+++ src/services/questionsService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const getQuestions = async () => {
-    const response = await axios.get('https://quizapi.io/api/v1/questions?apiKey=FFyDoQCl8TLjMC5stA9FdbiOvWSmcVmqjJHCAaZX&limit=10')
+    const response = await axios.get('https://quizapi.io/api/v1/questions?apiKey=-----------------&limit=10')
     return response.data;
 }
 
